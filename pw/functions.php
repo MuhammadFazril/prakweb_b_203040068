2cfe00f6280e72679a4bdd10f03cdd573c91eb3d
Fazril235 <203040068@mail.unpas.ac.id>
2022-09-30 15:06:38 +0700

    mengubah judul halaman

--- pw/functions.php
+++ pw/functions.php
@@ -22,5 +22,5 @@ function query($query)
   }
   return $rows;
 }
-
 ?>
+
--- pw/index.php
+++ pw/index.php
@@ -10,7 +10,7 @@ $buku = query("SELECT * FROM buku");
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Daftar Buku</title>
+  <title>Daftar Bubu</title>
   <link rel="stylesheet" href="css/style.css" class="css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
