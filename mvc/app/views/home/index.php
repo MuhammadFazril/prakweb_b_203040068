750517d7483039bce5f105da0d2bbd9beb7ac3d4
Fazril235 <arielff54@gmail.com>
2022-10-14 11:40:13 +0700

    Menyelesaikan video assets

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,3 +1,12 @@
-<h1>Selamat datang di Website Saya</h1>
+<div class="container">
 
+<div class="jumbotron">
 
+<h1 class="display-4">Selamat datang di Website Saya</h1>
+<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum iusto exercitationem deserunt autem recusandae aspernatur repellat, odit neque labore doloremque dicta optio asperiores reprehenderit possimus blanditiis laborum quam! Ducimus, minima.</p>
+<hr class="my-4">
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque itaque eius molestiae iure nulla ea deleniti debitis culpa molestias quasi nisi, fugit architecto dolorum. Praesentium tenetur iusto enim officia magnam.</p>
+
+<button type="button" class="btn btn-primary">Learn more</button>
+</div>
+</div>
